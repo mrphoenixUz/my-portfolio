9d530084b5951f0c030db869ab95c1e49d8d09ca
mrphoenixuz <karimovmoff@gmail.com>
2024-12-16 18:05:15 +0500

    photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,10 @@ const Hero = () => {
       id="home"
       className="min-h-screen flex py-20 xl:flex-row flex-col items-center mx-auto px-48 container gap-10"
     >
-      <div className="items-center justify-center h-[400px] w-[400px] rounded-full border-[#15ff00] border-8">
-        <img src={hero} alt="" className=" h-full object-cover" />
-      </div>
+      <div className="items-center justify-center h-[400px] w-[400px] rounded-full border-[#15ff00] border-8 overflow-hidden">
+  <img src={hero} alt="" className="h-full w-full object-cover" />
+</div>
+
       <div className="flex-1">
         <div className="md:text-left text-center">
           <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
